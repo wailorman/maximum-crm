35509f5e66abd5d76e967766e8978d61292d6ba7
wailorman <wailorman@gmail.com>
2014-10-27 23:55:04 +0400

    Начинаю билдить

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,56 +9,18 @@ module.exports = function (grunt) {
                 separator: '\n\n\n\n\n\n\n\n'
             },
             scripts: {
-                src: [
-                    'js/app.js',
-
-                    'js/modules/modals/*.js',
-                    'js/modules/group/*.js',
-                    'js/modules/lesson/*.js',
-                    'js/modules/feed.js'//,
-
-                    //'js/templates.js'
-                ],
-                dest: 'libs/built.js'
-            },
-            bower: {
-                src: [
-                    'js/bower/jquery-1.11.0.js',
-                    'js/bower/bootstrap.min.js',
-                    'js/bower/angular.min.js',
-                    'js/bower/angular-route.min.js',
-                    'js/bower/ng-tags-input.min.js',
-                    'js/bower/ui-bootstrap-tpls-0.11.2.js',
-                    'js/bower/plugins/metisMenu/metisMenu.min.js',
-                    'js/bower/sb-admin-2.js'
-                ],
-                dest: 'libs/bower.json'
+                src: 'src/js/**.js',
+                dest: 'build/build.js'
             },
             styles: {
-                src: [
-                    'css/bootstrap.min.css',
-                    "css/sb-admin-2.css",
-
-                    "css/ng-tags-input.min.css",
-                    "css/ng-tags-input.bootstrap.min.css",
-
-                    'css/my.css',
-                    'css/modules/feed.css',
-                    'css/modules/lesson/lesson-info.css',
-                    'css/modules/lesson/lesson-edit.css',
-                    'css/modules/lesson/lesson-new.css',
-                    'css/modules/lesson/lessons-list.css',
-
-                    'css/modules/group-info/group-info.css',
-                    'css/modules/group-info/group-info-edit.css'
-                ],
-                dest: 'libs/built.css'
+                src: 'src/css/**.css',
+                dest: 'build/build.css'
             }
         },
         uglify: {
-            stable: {
+            dev: {
                 files: {
-                    'libs/built.min.js': 'libs/built.js'
+                    'build/build.min.js': 'build/build.js'
                 }
             }
         },
@@ -66,9 +28,9 @@ module.exports = function (grunt) {
             options: {
                 keepSpecialComments: '0'
             },
-            stable: {
+            dev: {
                 files: {
-                    'libs/built.min.css': 'libs/built.css'
+                    'build/build.min.css': 'build/build.css'
                 }
             }
         },
@@ -84,29 +46,36 @@ module.exports = function (grunt) {
                 dest: 'build/index.html',
                 options: {
                     scripts: {
-                        built: 'libs/built.js',
+                        build: 'build/build.min.js',
                         angular: [
                             'bower_components/angular/angular.min.js',
-                            'bower_components/angular/angular.min.js.map',
                             'bower_components/angular-bootstrap/ui-bootstrap.min.js',
                             'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-                            'bower_components/angular-mocks/angular-route.min.js',
-                            'bower_components/angular-mocks/angular-route.min.js.map'
+                            'bower_components/angular-mocks/angular-route.min.js'
                         ],
                         bootstrap: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
-                        jquery: [
-                            'bower_components/jquery/dist/jquery.min.js',
-                            'bower_components/jquery/dist/jquery/jquery.min.map'
+                        jquery: 'bower_components/jquery/dist/jquery.min.js',
+                        sbadmin: [
+                            'bower_components/metisMenu/dist/jquery.metisMenu.min.js',
+                            'bower_components/sb-admin-v2/js/sb-admin.js'
                         ]
                     },
                     styles: {
-                        built: 'libs/built.min.css',
-                        bootstrap: [
-                            'bower_components/bootstrap/dist/css/bootstrap.min.css',
-                            'bower_components/bootstrap/dist/css/bootstrap.css.map'
+                        build: 'build/build.min.css',
+                        bootstrap: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+                        sbadmin: [
+                            'bower_components/metisMenu/dist/metisMenu.min.css',
+                            'bower_components/sb-admin-v2/css/sb-admin.css'
                         ]
                     }
                 }
+            }
+        },
+        copy: {
+            main: {
+                file: [
+                    { expand: true, src: ['bower_components/'], dest: '' }
+                ]
             }
         }
     });
@@ -116,10 +85,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-angular-templates');
     grunt.loadNpmTasks('grunt-html-build');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-auto-install');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
 
     grunt.registerTask('default', ['ngtemplates', 'concat', 'cssmin', 'uglify', 'htmlbuild']);
+    grunt.registerTask('lets-build', ['concat', 'cssmin', 'uglify', 'htmlbuild']);
     grunt.registerTask('no-templates', ['concat', 'cssmin', 'uglify', 'htmlbuild']);
 
 };
