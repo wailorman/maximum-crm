91fc93e0aa68f619ff0ab0008f8db837da8cfd1e
wailorman <wailorman@gmail.com>
2014-10-27 23:08:24 +0400

    Изменил контроллеры lesson

--- src/js/lesson/lesson-info.js
+++ src/js/lesson/lesson-info.js
@@ -1,29 +1,28 @@
-function LessonInfoCtrl($scope, $rootScope) {
+angular.module('maximumCrm')
+    .controller('LessonInfoCtrl', ['$scope', '$rootScope',
+        function LessonInfoCtrl($scope, $rootScope) {
 
-    $rootScope.curPageName = 'Информация о занятии';
+            $rootScope.curPageName = 'Информация о занятии';
 
-    $scope.lessonInfo = {
-        id: "abd9789abd9adb789abc978dc567",
-        group: {
-            name: 'Дети-12',
-            id: "abd9789abd9adb789abc978dc567"
-        },
-        coach: {
-            name: 'Пупкин Василий Иванович',
-            id: 'abd9789abd9adb789abc978dc567'
-        },
-        hall: {
-            name: "Ирландский зал",
-            id: "abd9789abd9adb789abc978dc567"
-        },
-        time: {
-            date: "24.01.2015",
-            start: "15:00",
-            end: "17:00"
-        }
-    };
+            $scope.lessonInfo = {
+                id: "abd9789abd9adb789abc978dc567",
+                group: {
+                    name: 'Дети-12',
+                    id: "abd9789abd9adb789abc978dc567"
+                },
+                coach: {
+                    name: 'Пупкин Василий Иванович',
+                    id: 'abd9789abd9adb789abc978dc567'
+                },
+                hall: {
+                    name: "Ирландский зал",
+                    id: "abd9789abd9adb789abc978dc567"
+                },
+                time: {
+                    date: "24.01.2015",
+                    start: "15:00",
+                    end: "17:00"
+                }
+            };
 
-}
-
-//angular.module('maximumCrm')
-crm.controller('LessonInfoCtrl', ['$scope', '$rootScope', LessonInfoCtrl()]);
+        }]);
