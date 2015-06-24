c651cd7f99e34d531da39361cab985450654a37c
wailorman <wailorman@gmail.com>
2015-06-24 17:24:39 +0300

    feat(api.lessons): Add populateArray()
    This take a resource object, request a several objects (keys are getting from arrayOfIds) and return (by promise) populated array

--- test/unit/api/lessons-resource.test.js
+++ test/unit/api/lessons-resource.test.js
@@ -1,12 +1,52 @@
 fdescribe( 'Lessons resource', function () {
 
-    var Lessons;
+    var Lessons, $httpBackend, $resource,
+        apiUri = 'http://api.max-crm.wailorman.ru:21080',
+        callback = {
+            error: function () {
+
+            },
+            success: function () {
+
+            },
+            notify: function (  ) {
+
+            }
+        };
+
+    /**
+     *
+     * @param {string} method
+     * @param {number} status
+     * @param {string} path
+     * @param {object} [data]
+     */
+    var defineRespond = function ( method, status, path, data ) {
+
+        $httpBackend.when( method, apiUri + path )
+            .respond( status, data, {} );
+
+    };
+
+    /**
+     *
+     * @param {string} method
+     * @param {string} path
+     * @param {object} [data]
+     */
+    var expectRequest = function ( method, path, data ) {
+
+        $httpBackend.expect( method, apiUri + path, data );
+
+    };
 
     beforeEach( module( 'starter.api.lessons' ) );
 
-    beforeEach( inject( function ( _Lessons_ ) {
+    beforeEach( inject( function ( _Lessons_, _$httpBackend_, _$resource_ ) {
 
         Lessons = _Lessons_;
+        $httpBackend = _$httpBackend_;
+        $resource = _$resource_;
 
     } ) );
 
@@ -266,6 +306,121 @@ fdescribe( 'Lessons resource', function () {
                 ).toThrow( new Error( "Not enough params (date missing)" ) );
 
             } );
+
+        } );
+
+    } );
+
+    describe( 'populateArray', function () {
+
+        var arrayToPopulate,
+            coachesResource;
+
+        beforeEach( function () {
+            spyOn( callback, 'error' );
+            spyOn( callback, 'success' );
+            spyOn( callback, 'notify' );
+        } );
+
+        beforeEach( function () {
+
+            arrayToPopulate = ['coach1', 'coach2'];
+
+            defineRespond( 'GET', 200, '/coaches/coach1', {
+                _id: 'coach1',
+                name: 'The Coach 1'
+            } );
+
+            defineRespond( 'GET', 200, '/coaches/coach2', {
+                _id: 'coach2',
+                name: 'The Coach 2'
+            } );
+
+            defineRespond( 'GET', 404, '/coaches/coach3' );
+            defineRespond( 'GET', 404, '/coaches/coach4' );
+
+            coachesResource = $resource( apiUri + '/coaches/:id', null,
+                {
+                    'get': { method: 'GET' }
+                }
+            );
+
+        } );
+
+        it( 'should be called and requested two objects from Coaches resource', function () {
+
+            Lessons.populateArray( coachesResource, arrayToPopulate )
+                .then( callback.success, callback.error );
+
+            $httpBackend.flush();
+
+            expectRequest( 'GET', '/coaches/coach1' );
+            expectRequest( 'GET', '/coaches/coach2' );
+
+            expect( callback.error ).not.toHaveBeenCalled();
+            expect( callback.success ).toHaveBeenCalled();
+
+        } );
+
+        it( 'should call callback.success with array of populated coaches', function () {
+
+            Lessons.populateArray( coachesResource, arrayToPopulate )
+                .then( callback.success, callback.error );
+
+            $httpBackend.flush();
+
+            expect( callback.success.calls.mostRecent().args[0] instanceof Array ).toBeTruthy();
+
+            expect( callback.success.calls.mostRecent().args[0][0]._id ).toEqual( 'coach1' );
+            expect( callback.success.calls.mostRecent().args[0][1]._id ).toEqual( 'coach2' );
+
+            expect( callback.success.calls.mostRecent().args[0][0].name ).toEqual( 'The Coach 1' );
+            expect( callback.success.calls.mostRecent().args[0][1].name ).toEqual( 'The Coach 2' );
+
+        } );
+
+        it( 'should call callback.notify and callback.success too if one of two objects can not found', function () {
+
+            arrayToPopulate = [ 'coach1', 'coach3' ];
+
+            Lessons.populateArray( coachesResource, arrayToPopulate )
+                .then( callback.success, callback.error, callback.notify );
+
+            $httpBackend.flush();
+
+            expectRequest( 'GET', '/coaches/coach1' );
+            expectRequest( 'GET', '/coaches/coach3' );
+
+            expect( callback.notify ).toHaveBeenCalled();
+            expect( callback.success ).toHaveBeenCalled();
+
+            expect( callback.notify.calls.count() ).toEqual( 1 );
+            expect( callback.notify.calls.mostRecent().args[0] ).toEqual( "Can't find coach3" );
+
+        } );
+
+        it( 'should call callback.error and callback.notify if 2/2 objects responds with 404', function () {
+
+            arrayToPopulate = [ 'coach3', 'coach4' ];
+
+            Lessons.populateArray( coachesResource, arrayToPopulate )
+                .then( callback.success, callback.error, callback.notify );
+
+            $httpBackend.flush();
+
+            expectRequest( 'GET', '/coaches/coach3' );
+            expectRequest( 'GET', '/coaches/coach4' );
+
+            expect( callback.success ).not.toHaveBeenCalled();
+            expect( callback.error ).toHaveBeenCalled();
+            expect( callback.notify ).toHaveBeenCalled();
+
+            expect( callback.notify.calls.count() ).toEqual( 2 );
+            expect( callback.error.calls.count() ).toEqual( 1 );
+
+            expect( callback.error.calls.mostRecent().args[0] ).toEqual( "Can't find any object" );
+
+            // I wont check notify callback args bcz I made sure it calls correctly in previous test
 
         } );
 
--- www/js/resources/lessons.js
+++ www/js/resources/lessons.js
@@ -1,7 +1,7 @@
 angular.module( 'starter.api.lessons', [
     'ngResource'
 ] )
-    .factory( 'Lessons', function ( $resource ) {
+    .factory( 'Lessons', function ( $resource, $q ) {
 
         var apiUrl = 'http://api.max-crm.wailorman.ru:21080';
 
@@ -86,6 +86,58 @@ angular.module( 'starter.api.lessons', [
             resultTime.end = new Date( resultTime.start.getTime() + timeObject.duration * 60 * 1000 );
 
             return resultTime;
+        };
+
+        /**
+         * Get populated array by array of IDs.
+         * Async function
+         *
+         * Method don't stop working if it can't find some of objects.
+         * On every error response (e.g. 404) it will call deferred.notify.
+         * So, if it can't find two objects, it will calls notify function twice.
+         * But! If method can't find all of requested objects it will call reject
+         *
+         * So
+         * notify -- calls on every error
+         * reject -- calls only if all requests respond an error
+         * resolve -- calls after all requests been responded. Args: resultArray (already populated)
+         *
+         * @param {Resource} resource
+         * @param {array|Array} arrayOfIds
+         */
+        Lessons.populateArray = function ( resource, arrayOfIds ) {
+            var deferred = $q.defer(),
+                resultArray = [],
+                numberOfErrorResponds = 0;
+
+            async.each(
+                arrayOfIds,
+                function ( objectId, ecb ) {
+
+                    resource.get( { id: objectId } ).$promise
+                        .then(
+                        function ( coach ) {
+                            resultArray.push( coach );
+                            ecb();
+                        },
+                        function () {
+                            numberOfErrorResponds++;
+                            deferred.notify( "Can't find " + objectId );
+                            ecb();
+                        }
+                    );
+
+                },
+                function () {
+                    if ( numberOfErrorResponds == arrayOfIds.length ) {
+                        deferred.reject( "Can't find any object" );
+                    } else if ( numberOfErrorResponds < arrayOfIds.length && resultArray ) {
+                        deferred.resolve( resultArray );
+                    }
+                }
+            );
+
+            return deferred.promise;
         };
 
         return Lessons;
